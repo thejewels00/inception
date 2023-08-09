f37810dc59ea632f9b4f4e764e99d8f63fc7c812
thejewles00 <jchennak@student.1337.ma>
2023-08-09 05:56:57 +0100

    working in wp

--- srcs/requirements/wordpress/scripts/script.sh
+++ srcs/requirements/wordpress/scripts/script.sh
@@ -1,8 +1,20 @@
 #!/bin/bash
 
+chown -R www-data:www-data /var/www/html/*
+
+sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
+
 cd /var/www/html/
+
+curl -O https://wordpress.org/latest.tar.gz
+tar -xzf latest.tar.gz 
+rm -rf latest.tar.gz
+chown -R www-data:www-data /var/www/html/wordpress/*
+cd /var/www/html/wordpress
+
+
 #dowlanding wordpress files 
-wp core download --allow-root --path=/var/www/html/
+#wp core download --allow-root --path=/var/www/html/
 
 #bach yb9a mhangi mhangi mrani :D
 exec php-fpm7.3 -F -R
