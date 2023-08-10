0b2d7d5cd56196ddecc67d1e6edd97a9c3746bb1
thejewles00 <jchennak@student.1337.ma>
2023-08-10 13:55:11 +0100

    docker compose

--- srcs/requirements/wordpress/scripts/script.sh
+++ srcs/requirements/wordpress/scripts/script.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
+
+mkdir -p /var/www/html/
+mkdir -p /run/php
+
 chown -R www-data:www-data /var/www/html/*
+chown -R www-data:www-data /run/*
 
 sed -i 's/listen = \/run\/php\/php7.3-fpm.sock/listen = 9000/' /etc/php/7.3/fpm/pool.d/www.conf
 
 cd /var/www/html/
 
 curl -O https://wordpress.org/latest.tar.gz
-tar -xzf latest.tar.gz 
+tar -xzf latest.tar.gz
 rm -rf latest.tar.gz
 chown -R www-data:www-data /var/www/html/wordpress/*
 cd /var/www/html/wordpress
@@ -16,5 +21,5 @@ cd /var/www/html/wordpress
 #dowlanding wordpress files 
 #wp core download --allow-root --path=/var/www/html/
 
-#bach yb9a mhangi mhangi mrani :D
+#bach yb9a mhangi  mrani :D
 exec php-fpm7.3 -F -R
